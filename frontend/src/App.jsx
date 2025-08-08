e5baaad233c6d15824463aae6f1c60ab42fbc05e
Gavin <piperga@oregonstate.edu>
2025-08-07 20:41:49 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import { Routes, Route } from 'react-router-dom';
 // Pages
 import Home from './pages/Home';
 import Games from './pages/Games';
+import DBCart from './pages/Cart';
 import DBCustomers from './pages/Customers'
+import DBLibrary from './pages/Library'
 import DBGenres from './pages/Genres'
 import DBPurchases from './pages/Purchases'
 import DBPlatforms from './pages/Platforms'
@@ -13,7 +15,7 @@ import DBPlatforms from './pages/Platforms'
 import Navigation from './components/Navigation';
 
 // Define the backend port and URL for API requests
-const backendPort = 9624;  // Use the port you assigned to the backend server, this would normally go in a .env file
+const backendPort = 9660;  // Use the port you assigned to the backend server, this would normally go in a .env file
 const backendURL = `http://classwork.engr.oregonstate.edu:${backendPort}`;
 
 function App() {
@@ -24,7 +26,9 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home backendURL={backendURL}/>} />
                 <Route path="/Games" element={<Games backendURL={backendURL} />} />
-                <Route path="/Customers" element={<DBCustomers backendURL={backendURL} />} />
+                <Route path="/customers/:customerID/cart" element={<DBCart backendURL={backendURL}/>} />
+                <Route path="/Customers" element={<DBCustomers backendURL={backendURL} />} />\
+                <Route path="/customers/:customerID/library" element={<DBLibrary backendURL={backendURL} />} />
                 <Route path="/Purchases" element={<DBPurchases backendURL={backendURL} />} />
                 <Route path="/Platforms" element={<DBPlatforms backendURL={backendURL} />} />
                 <Route path="/Genres" element={<DBGenres backendURL={backendURL} />} />
